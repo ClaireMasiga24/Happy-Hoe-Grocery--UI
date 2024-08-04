50a6d563c077f67c9653d80c554ba9ef779dd0ac
Claire Masiga <ClaireMasiga22@gmail.com>
2024-08-04 19:45:04 +0300

    First commit

---
+++ Scripts/grocery.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const slides = document.querySelectorAll(".banner-slider img");
+    let currentSlide = 0;
+
+    function showSlide(index) {
+        slides.forEach((slide, i) => {
+            slide.style.transform = `translateX(${(i - index) * 100}%)`;
+        });
+    }
+
+    function nextSlide() {
+        currentSlide = (currentSlide + 1) % slides.length;
+        showSlide(currentSlide);
+    }
+
+    setInterval(nextSlide, 3000); // Change slide every 3 seconds
+
+    showSlide(currentSlide);
+});
